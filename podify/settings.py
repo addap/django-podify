29d96702b8d2b9da85431f3028f33790eec333ed
Adrian Dapprich <adrian.da@outlook.com>
2019-02-22 22:49:04 +0100

    change static root

--- podify/settings.py
+++ podify/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # MEDIA_ROOT = '/home/adrian/programming/django-podify/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
