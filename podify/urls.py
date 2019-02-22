2929588bc8b7124432530e9639e5df2078afa5d2
Adrian Dapprich <adrian.da@outlook.com>
2019-02-22 18:30:19 +0100

    favicon redirect so that it also works with admin panel

--- podify/urls.py
+++ podify/urls.py
@@ -14,12 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 # todo the + static(...) line is to be only used in development
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico',
+         RedirectView.as_view(
+             url='static/favicon.png',
+             permanent=False),
+         name="favicon"
+         ),
     path('', include('podcasts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
