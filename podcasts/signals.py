690289d3cfab9b5a74a4b8f846be77b56a32f98b
Adrian Dapprich <adrian@dapprich.dev>
2020-04-06 22:04:15 +0200

    add logs to signals

--- podcasts/signals.py
+++ podcasts/signals.py
@@ -12,6 +12,7 @@ from podify.settings import MEDIA_ROOT
 @receiver(pre_delete, sender=Podcast, dispatch_uid="deleting podcast files")
 def podcast_delete_files(sender, **kwargs):
     podcast = kwargs['instance']
+    print(f'Podcast {podcast.name} will be deleted')
     # remote everything from that podcast, the episode signals will then do nothing
     shutil.rmtree(os.path.join(MEDIA_ROOT, podcast.slug))
 
@@ -19,6 +20,7 @@ def podcast_delete_files(sender, **kwargs):
 @receiver(pre_delete, sender=Episode, dispatch_uid="deleting episode files")
 def episode_delete_files(sender, **kwargs):
     episode = kwargs['instance']
+    print(f'Episode {episode.name} will be deleted')
     if episode.mp3 and episode.mp3.storage.exists(episode.mp3.name):
         os.remove(episode.mp3.path)
     if episode.image and episode.image.storage.exists(episode.image.name):
@@ -27,10 +29,9 @@ def episode_delete_files(sender, **kwargs):
 
 @receiver(pre_enqueue)
 def my_pre_enqueue_callback(sender, task, **kwargs):
-    print("Task {} will be enqueued".format(task["name"]))
+    print(f'Task {task["name"]} will be enqueued')
 
 
 @receiver(pre_execute)
 def my_pre_execute_callback(sender, func, task, **kwargs):
-    print("Task {} will be executed by calling {}".format(
-        task["name"], func))
+    print(f'Task {task["name"]} will be executed by calling {func}')
