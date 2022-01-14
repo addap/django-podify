67b2c89d87b22572b56245cb4f272e527265b246
Adrian Dapprich <adrian@dapprich.dev>
2022-01-14 16:19:01 +0100

    change url. I hope podcatcher can recognize it now

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('sync', views.podcast_sync_all, name='podcast-sync-all'),
     path('podcast/<slug:slug>/', views.podcast_detail, name='podcast-detail'),
     path('podcast/<slug:slug>/sync', views.podcast_sync, name='podcast-sync'),
-    path('podcast/<slug:slug>/rss', PodcastFeed(), name='podcast-rss'),
+    path('podcast/<slug:slug>/rss.xml', PodcastFeed(), name='podcast-rss'),
     path('podcast/<slug:slug>/dummy-episode-sync', views.dummy_episode_sync, name='dummy-episode-sync'),
     path('podcast/<slug:slug>/ep/<slug:episode_slug>', views.episode_download, name='episode-download'),
 ]
